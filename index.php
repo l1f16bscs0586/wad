6f2f24ef11ba6f3245f54d8f600607f56c97ffe0
l1f16bscs0586 <44543565+l1f16bscs0586@users.noreply.github.com>
2019-01-03 23:36:04 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,132 +1,149 @@
 <?php
-require_once("server/db_connection.php");
-include_once("server/functions.php");
+require_once "db_connection.php";
+if(!isset($_POST['insert_products']))
+{
+    $pro_title =isset($_POST['pro_title']) ? $_POST['pro_title'] : '';
+    $pro_cat =isset($_POST['pro_cat']) ? $_POST['pro_cat'] : '';
+    $pro_brand =isset($_POST['pro_brand']) ? $_POST['pro_brand'] : '';
+    $pro_price =isset($_POST['pro_price']) ? $_POST['pro_price'] : '';
+    $pro_desc =isset($_POST['pro_desc']) ? $_POST['pro_desc'] : '';
+    $pro_keywords =isset($_POST['pro_kw']) ? $_POST['pro_kw'] : '';
+    $insertQuery = "insert into products(pro_title,pro_cat,pro_brand,pro_price,pro_desc,pro_keywords)
+    values('$pro_title','$pro_cat','$pro_brand','$pro_price','$pro_desc','$pro_keywords');";
+    //echo $insertQuery;
+    $res = mysqli_query($con,$insertQuery);
+    if(!$res)
+    {
+        echo "Not Executed";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Tech Box</title>
+    <title>Insert Product</title>
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <style>
+        * {
+            font-family: 'Old Standard TT', serif;
+        }
+    </style>
 </head>
 <body>
-
-<header class="container-fluid">
-    <div class="row">
-        <div class="col-12 no-padding">
-            <nav class="navbar navbar-light bg-light navbar-expand-sm fixed-top">
-                <a class="navbar-brand" href="index.php"><img src="media/logo.png" width="175" height="50" alt="logo">
-                </a>
-                <button class="navbar-toggler" type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse row" id="navbarToggler">
-                    <div class="col-lg-8 offset-lg-1 col-md-8 col-sm-7">
-                        <form class="form-inline">
-                            <div class="input-group">
-                                <input type="search" class="form-control"
-                                       id="search-bar" name="search"
-                                       placeholder="Find Mobile Phones, Laptops, and more..">
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary btn-lg" type="button"><i class="fas fa-search"></i></button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-sm-5">
-                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0 float-sm-right">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="index.php"><i class="fas fa-heart sc-color fa-2x"></i></a>
-                            </li>
-                            <li class="nav-item active">
-                                <a class="nav-link" href="index.php"><i class="fas fa-shopping-cart sc-color fa-2x"></i></a>
-                            </li>
-                            <li class="nav-item active">
-                                <a class="nav-link" href="index.php"> <span class="sc-fs">Login </span></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </div>
-
-</header>
-<div class="wrapper">
-    <!-- Sidebar  -->
-    <nav id="sidebar" class="bg-light">
-        <ul class="list-unstyled components">
-            <li class="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle nav-link">
-                    <i class="fas fa-sitemap"></i>
-                    Categories
-                </a>
-                <ul class="collapse show list-unstyled" id="homeSubmenu">
-
-                    <?php
-                    $rows = getCategories();
-                    for ($i = 0; $i < mysqli_num_rows($rows); $i++) {
-                        $row = mysqli_fetch_assoc($rows);
-                        echo "<li> <a class='nav-link'  href='#'>" . $row['name'] . "</a></li>" ;
-                    }
-                    ?>
-
-                </ul>
-            </li>
-            <li class="active">
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle nav-link">
-                    <i class="fas fa-briefcase"></i>
-                    Brands
-                </a>
-                <ul class="collapse show list-unstyled" id="pageSubmenu">
-                    <?php
-                    $rows = getBrands();
-                    for ($i = 0; $i < mysqli_num_rows($rows); $i++) {
-                        $row = mysqli_fetch_assoc($rows);
-                        echo "<li> <a class='nav-link'  href='#'>" . $row['name'] . "</a></li>" ;
-                    }
-                    ?>
-                </ul>
-            </li>
-            <li>
-                <a class="nav-link"  href="#">
-                    <i class="fas fa-question"></i>
-                    FAQ
-                </a>
-            </li>
-            <li>
-                <a class="nav-link"  href="#">
-                    <i class="fas fa-paper-plane"></i>
-                    Contact
-                </a>
-            </li>
-        </ul>
-    </nav>
-    <article id="content" class="container-fluid bg-white">
-
+<div class="container">
+    <h1 class="text-center my-4"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New </span> Product </h1>
+    <form>
         <div class="row">
-            <div class="col">
-                Content
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_title" class="float-md-right"> <span class="d-sm-none d-md-inline"> Product </span> Title:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-file-signature"></i></div>
+                    </div>
+                    <input type="text" class="form-control" id="pro_title" name="pro_title" placeholder="Enter Product Title" >
+                </div>
+            </div>
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_cat" class="float-md-right"><span class="d-sm-none d-md-inline"> Product </span> Category:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4 mt-3 mt-lg-0">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-list-alt"></i></div>
+                    </div>
+                    <select class="form-control" id="pro_cat" name="pro_cat">
+                        <option>Select Category</option>
+                        <option>Mobile</option>
+                        <option>Laptop</option>
+                        <option>Tablet</option>
+                        <option>Watch</option>
+                        <option>Camera</option>
+                    </select>
+                </div>
             </div>
         </div>
-    </article>
-
-
-</div>
-<footer class="container-fluid">
-    <div class="row">
-        <div class="col text-center">
-            &copy; 2019 by Muhammad Ali Makhdoom
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_brand" class="float-md-right"> <span class="d-sm-none d-md-inline"> Product </span> Brand:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-stamp"></i></div>
+                    </div>
+                    <select class="form-control" id="pro_brand" name="pro_brand">
+                        <option>Select Brand</option>
+                        <option>Apple</option>
+                        <option>Samsung</option>
+                        <option>Oppo</option>
+                        <option>Dell</option>
+                        <option>HP</option>
+                        <option>Sony</option>
+                    </select>
+                </div>
+            </div>
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_img" class="float-md-right"><span class="d-sm-none d-md-inline"> Product </span> Image:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4 mt-3 mt-lg-0">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="far fa-image"></i></div>
+                    </div>
+                    <input class="form-control" type="file" id="pro_img" name="pro_img">
+                </div>
+            </div>
         </div>
-    </div>
-</footer>
-<script src="js/jquery-3.3.1.js"></script>
-<script src="js/bootstrap.bundle.js"></script>
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_price" class="float-md-right"> <span class="d-sm-none d-md-inline"> Product </span> Price:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-money-bill"></i></div>
+                    </div>
+                    <input class="form-control" id="pro_price" name="pro_price" placeholder="Enter Product Price">
+                </div>
+            </div>
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_kw" class="float-md-right"><span class="d-sm-none d-md-inline"> Product </span> Keyword:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4 mt-3 mt-lg-0">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-key"></i></div>
+                    </div>
+                    <input class="form-control" type="text" id="pro_kw" name="pro_kw" placeholder="Enter Product Keywords">
+                </div>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_desc" class="float-md-right"><span class="d-sm-none d-md-inline"> Product </span> Detail:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="far fa-comment-alt"></i></div>
+                    </div>
+                    <textarea class="form-control" type="file" id="pro_desc" name="pro_desc" placeholder="Enter Product Detail"></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto"></div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <button type="submit" name="insert_products" class="btn btn-primary btn-block"><i class="fas fa-plus"></i> Insert Now </button>
+            </div>
+        </div>
+    </form>
+</div>
 </body>
 </html>
